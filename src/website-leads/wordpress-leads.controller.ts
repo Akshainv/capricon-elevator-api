0be6a9565129ca8808b5ba7eda7c5c14dc49a4dc
Akshai N V <akshaiachu012@gmail.com>
2026-02-27 13:24:25 +0530

    website leads

--- src/website-leads/wordpress-leads.controller.ts
+++ src/website-leads/wordpress-leads.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Post, Body, Req } from '@nestjs/common';
 import { WebsiteLeadsService } from './website-leads.service';
-import { Request } from 'express';
+import * as express from 'express';
 
 @Controller('api/leads')
 export class WordPressLeadsController {
     constructor(private readonly websiteLeadsService: WebsiteLeadsService) { }
 
     @Post('website')
-    async createWebsiteLead(@Body() body: any, @Req() req: Request) {
+    async createWebsiteLead(@Body() body: any, @Req() req: express.Request) {
         console.log('--- 🚀 Incoming Request to /api/leads/website ---');
         console.log('Headers:', JSON.stringify(req.headers, null, 2));
         console.log('Body:', JSON.stringify(body, null, 2));
@@ -15,4 +15,5 @@ export class WordPressLeadsController {
         return this.websiteLeadsService.createWebsiteLead(body);
     }
 }
+
 
